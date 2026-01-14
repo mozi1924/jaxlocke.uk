d9a753d3f09dd99eb69cece9b644ac3242ff44fa
mozi1924 <15985142983@163.com>
2026-01-14 08:08:56 +0800

    add a 404 page

---
+++ main-404.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NotFound from './NotFound'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <NotFound />
+  </React.StrictMode>,
+)
